fe6674240f814d5732c2ff7b1d37c5c742ae368b
Tehpson <jesper@familjen-pettersson.se>
2021-06-10 17:37:23 +0200

    started up

---
+++ Controllers/UserTrainingDataController.cs
@@ -0,0 +1,15 @@
+﻿namespace Workify_Backend.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTrainingDataController : ControllerBase
+    {
+    }
+}
