0d8093359c565e05e9620d7e63ea187f7d116b9a
nilsp <nilsp@DESKTOP-U0GS61K>
2021-06-11 01:43:45 +0200

    Jobbat med databas

---
+++ Database/Database.cs
@@ -0,0 +1,31 @@
+﻿namespace Workify_Backend.Database
+{
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.IO;
+
+    public class Database : DbContext
+    {
+        static string DatabaseFile { get; set; } = @".\Workifydb.db";
+
+        public DbSet<Models.User> Users { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var path = Environment
+                .GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            path = Path.Combine(path, "Workify");
+                Directory.CreateDirectory(path);
+                path = Path.Combine(path, DatabaseFile);
+                optionsBuilder
+                .UseSqlite(@"Data Source = " + path + ";");
+        }
+    }
+    
+}
