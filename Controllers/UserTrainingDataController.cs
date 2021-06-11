8028ec9253dd49fefcd34842788eece6ffba9dfe
Tehpson <jesper@familjen-pettersson.se>
2021-06-11 15:41:36 +0200

    start on creating usertraning

--- Controllers/UserTrainingDataController.cs
+++ Controllers/UserTrainingDataController.cs
@@ -1,6 +1,7 @@
 ﻿namespace Workify_Backend.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
 
     [ApiController]
     public class UserTrainingDataController : ControllerBase
@@ -17,6 +18,43 @@
         [Route("api/user/{userId}/WorkoutData/{workoutid}")]
         public IActionResult GetAll(string userId, int workoutid)
         {
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("api/user/{userId}/WorkoutData")]
+        public IActionResult Post(ulong userID, Models.UserTraining userTraining)
+        {
+            using (var db = new Database.WorkifyDatabase())
+            {
+                var user = db.Users.FirstOrDefault(x => x.Id == userID);
+                if(user == null)
+                {
+                    return NotFound("user Not found");
+                }
+            }
+
+            if(userTraining.Title == null)
+            {
+                return Problem("Title need to be specified");
+            }
+            else if (userTraining.Title.Length > 25)
+            {
+                return Problem("title to long");
+            }
+            else if(userTraining.Time == null)
+            {
+                return Problem("Time need to be specified");
+            }
+            else if(userTraining.Comment.Length > 250)
+            {
+                return Problem("comment to long");
+            }
+            else
+            {
+                var dateteim = System.DateTime.Now;
+
+            }
             return NotFound();
         }
     }
